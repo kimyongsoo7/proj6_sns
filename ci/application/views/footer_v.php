b237fc23ecec22003413736df29148cf977226bc
DESKTOP-KARS9C3\IGAW <a01097672147@gmail.com>
2018-10-05 10:19:44 +0900

    문구수정

---
+++ ci/application/views/footer_v.php
@@ -0,0 +1,12 @@
+
+<footer id="footer">
+    <blockquote>
+        <p><a class="azubu" href="http://www.cikorea.net/" target="blank">CodeIgniter포럼</a></p>
+        <small>Copyright by <em class="black"><a href="mailto:mold76@hanmail.net">Louis.Kim</a></small>
+    </blockquote>
+</footer>
+
+</div>
+
+</body>
+</html>
--- ci/application/views/header_v.php
+++ ci/application/views/header_v.php
@@ -9,15 +9,16 @@
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <link rel='stylesheet' href="/todo/include/css/bootstrap.css" />
+        <!--<link rel='stylesheet' href="/todo/include/css/bootstrap.css" />-->
+        <link rel='stylesheet' href="/include/css/bootstrap.css" />
     </head>
     <body>
         <div id="main">
             
             <header id="header" data-role="header" data-position="fixed">
                 <blockquote>
-                    <p>만들면서 배우는 CodeIgniter</p>
-                    <small>실행 예제</small>
+                    <p>CodeIgniter</p>
+                    <small>예제</small>
                     <p>
 <?php
 /*
